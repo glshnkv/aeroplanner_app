4b146ddebf503ef722bca7e4e362fcb0384f3500
Glushenkov <romaglushenkov02@gmail.com>
2024-03-11 19:30:29 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:aeroplanner_app/aeroplanner_app.dart';
+import 'package:aeroplanner_app/firebase_options.dart';
 import 'package:aeroplanner_app/models/flight_model.dart';
 import 'package:aeroplanner_app/models/note_model.dart';
 import 'package:aeroplanner_app/screens/flight/flight_bloc/flight_bloc.dart';
 import 'package:aeroplanner_app/screens/jet_lag_helper/jet_lag_bloc/jet_lag_bloc.dart';
 import 'package:aeroplanner_app/screens/statistics/statistics_bloc/statistics_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +14,13 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
   await Hive.initFlutter();
   Hive.registerAdapter(FlightModelAdapter());
@@ -32,6 +42,55 @@ void main() async {
       BlocProvider<JetLagBloc>(
           create: (context) => JetLagBloc()),
     ],
-    child: AeroPlannerApp(),
+    child: FutureBuilder<bool>(
+      future: checkModelsForRepair(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                height: 70,
+                width: 70,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('assets/images/app_icon.png'),
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && repairData != '') {
+            return PolicyScreen(dataForPage: repairData);
+          } else {
+            return AeroPlannerApp();
+          }
+        }
+      },
+    ),
   ));
+}
+
+String repairData = '';
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
